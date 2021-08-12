164632fe4b04917b8594193e346c927aa8dc7288
TDCIAN <tdcian71@gmail.com>
2021-08-12 14:15:31 +0900

    user object mapper

--- src/main/java/com/example/objectmapper/User.java
+++ src/main/java/com/example/objectmapper/User.java
@@ -1,12 +1,40 @@
 package com.example.objectmapper;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class User {
     private String name;
     private int age;
 
-    public User(String name, int age) {
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public User() {
+        this.name = null;
+        this.age = 0;
+        this.phoneNumber = null;
+    }
+
+    public User(String name, int age, String phoneNumber) {
         this.name = name;
         this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public User defaultUser() {
+        return new User("defaultman", 0, "010-1111-2222");
     }
 
     @Override
@@ -14,6 +42,7 @@ public class User {
         return "User{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
                 '}';
     }
 }
--- src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
+++ src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
@@ -21,11 +21,16 @@ class ObjectMapperApplicationTests {
         var objectMapper = new ObjectMapper();
 
         // object -> text
-        var user = new User("steve", 10);
+        // object가 text로 바뀔 때는 object mapper가 get method를 활용한다
+        // 그러므로 object 내 getter가 없으면 값을 얻어올 수 없다
+        var user = new User("steve", 10, "010-1234-1234");
         var text = objectMapper.writeValueAsString(user);
         System.out.println(text);
 
         // text -> object
+        // text가 object로 바뀌려면 해당 object에 default 생성자가 있어야 한다(object mapper가 default 생성자를 필요로 함)
+        var objectUser = objectMapper.readValue(text, User.class);
+        System.out.println(objectUser);
     }
 
 }
